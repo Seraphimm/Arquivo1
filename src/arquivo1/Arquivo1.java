f1bb3d6aab31aca35ee998f45d40ae214fb004af
Aluno <Aluno@lab01-21>
2017-06-10 15:49:54 -0300

    git2

--- src/arquivo1/Arquivo1.java
+++ src/arquivo1/Arquivo1.java
@@ -15,7 +15,8 @@ public class Arquivo1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //teste
     }
     
 }
